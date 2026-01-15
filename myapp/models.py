92674bdbf00721abb6109ee0b2f69d616c5307be
kibetnathan <kibetnat@gmail.com>
2026-01-15 09:43:06 +0300

    register models

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,21 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {"message" : "Welcome to Django!"}
+    return render(request, 'index.html')
